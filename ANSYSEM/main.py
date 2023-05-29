152ee3dc2ffed84995be4eb34a8c50942c13bddf
Dong Hyun Kim <mwave73@gmail.com>
2023-05-29 19:14:01 +0900

    git push origin main

--- ANSYSEM/main.py
+++ ANSYSEM/main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('ANSYSEM 123456')
+    print_hi('ANSYSEM 1234567')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
